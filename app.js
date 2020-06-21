1d007b3116935427662cba82d6fc571da1f37133
Ezekiel <iamwebwiz@gmail.com>
2020-06-21 11:29:01 +0100

    add configurations and tld routes

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const connectDB = require('./config/database');
+
+// Load configuration
+dotenv.config({path: './.env'});
+
+connectDB();
+
+// Initialize application
+const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Handlebars configuration
+app.engine(
+  '.hbs',
+  handlebars({
+    extname: '.hbs',
+    defaultLayout: 'main',
+  })
+);
+app.set('view engine', '.hbs');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
