abde2b786239ba2193bd850e0152d5e710739919
Ezekiel <iamwebwiz@gmail.com>
2020-06-25 00:17:33 +0100

    add auth routes

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/google/callback', {});
+
+module.exports = router;
