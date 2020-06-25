44593f9da2966c6b9c35a67f40846902dd662e7b
Ezekiel <iamwebwiz@gmail.com>
2020-06-25 01:59:09 +0100

    update user model

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   googleId: { type: String, required: true },
   displayName: { type: String, required: true },
   firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  lastName: { type: String, required: false },
   image: { type: String },
   createdAt: { type: Date, default: Date.now() },
 });
