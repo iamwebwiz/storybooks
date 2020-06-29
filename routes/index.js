7aebd0ac789a6767235e787229bb4a2b6e5bff49
Ezekiel <iamwebwiz@gmail.com>
2020-06-29 02:56:57 +0100

    update index route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middlewares/auth');
+const Story = require('../models/Story');
 
 router.get('/', ensureGuest, (request, response) => {
   response.render('login', { layout: 'auth' });
 });
 
-router.get('/dashboard', ensureAuth, (request, response) => {
-  response.render('dashboard');
+router.get('/dashboard', ensureAuth, async (request, response) => {
+  try {
+    const stories = await Story.find({ user: request.user.id }).lean();
+  } catch (error) {}
+
+  response.render('dashboard', {
+    name: request.user.firstName,
+  });
 });
 
 module.exports = router;
