16e33414e3620e22b3cd25b43f60175c95574e88
Ezekiel <iamwebwiz@gmail.com>
2020-06-30 01:29:53 +0100

    update dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const router = express.Router();
-const { ensureAuth, ensureGuest } = require('../middlewares/auth');
-const Story = require('../models/Story');
+const express = require('express')
+const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middlewares/auth')
+const Story = require('../models/Story')
 
 router.get('/', ensureGuest, (request, response) => {
-  response.render('login', { layout: 'auth' });
-});
+  response.render('login', { layout: 'auth' })
+})
 
 router.get('/dashboard', ensureAuth, async (request, response) => {
   try {
-    const stories = await Story.find({ user: request.user.id }).lean();
-  } catch (error) {}
+    const stories = await Story.find({ user: request.user.id }).lean()
 
-  response.render('dashboard', {
-    name: request.user.firstName,
-  });
-});
+    response.render('dashboard', {
+      name: request.user.firstName,
+      stories,
+    })
+  } catch (error) {
+    console.error(error)
+  }
+})
 
-module.exports = router;
+module.exports = router
