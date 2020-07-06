0d035fd2e2afb3599ab7790f8c9f10e00702caa8
Ezekiel <iamwebwiz@gmail.com>
2020-07-06 03:12:05 +0100

    update auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,16 @@
 module.exports = {
   ensureAuth: function (request, response, next) {
     if (request.isAuthenticated()) {
-      return next();
+      return next()
     } else {
-      response.redirect('/');
+      response.redirect('/')
     }
   },
   ensureGuest: function (request, response, next) {
     if (request.isAuthenticated()) {
-      response.redirect('/dashboard');
+      response.redirect('/dashboard')
     } else {
-      return next();
+      return next()
     }
   },
-};
+}
