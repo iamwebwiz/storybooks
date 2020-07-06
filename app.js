b2b69bf2705ed90679ce2f91813a732255a8bec3
Ezekiel <iamwebwiz@gmail.com>
2020-07-06 07:32:57 +0100

    add select helper to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
@@ -23,13 +24,30 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
 // Handlebars helpers
-const { formatDate, truncate, stripTags, editIcon } = require('./helpers/hbs')
+const {
+  formatDate,
+  truncate,
+  stripTags,
+  editIcon,
+  select,
+} = require('./helpers/hbs')
 
 // Handlebars configuration
 app.engine(
@@ -37,7 +55,7 @@ app.engine(
   handlebars({
     extname: '.hbs',
     defaultLayout: 'main',
-    helpers: { formatDate, truncate, stripTags, editIcon },
+    helpers: { formatDate, truncate, stripTags, editIcon, select },
   })
 )
 app.set('view engine', '.hbs')
