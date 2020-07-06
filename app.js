6c7e6b043a788580ef41ec179824f1d0b4388282
Ezekiel <iamwebwiz@gmail.com>
2020-07-06 04:07:15 +0100

    add body parser middleware to app.js

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const path = require('path');
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const connectDB = require('./config/database');
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/database')
 
 // Load configuration
-dotenv.config({ path: './.env' });
+dotenv.config({ path: './.env' })
 
-require('./config/passport')(passport);
+require('./config/passport')(passport)
 
-connectDB();
+connectDB()
 
 // Initialize application
-const app = express();
+const app = express()
+
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
 
 // Handlebars configuration
@@ -31,8 +35,8 @@ app.engine(
     extname: '.hbs',
     defaultLayout: 'main',
   })
-);
-app.set('view engine', '.hbs');
+)
+app.set('view engine', '.hbs')
 
 // Sessions
 app.use(
@@ -42,21 +46,22 @@ app.use(
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
-);
+)
 
 // Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Static directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth'));
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+)
